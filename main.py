2ba955614be03c0d6c3caa2ed3223f45ea1428b2
dmitriys-pc <dmitriys@physicianschoice.com>
2025-09-05 13:53:30 +0300

    Update main script, add full logic for work with files.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import re
-import httpx
-import codecs
 import time
 import json
+import codecs
 import asyncio
+import httpx
 from pathlib import Path
 from loguru import logger
 from pymupdf import open as pdf_open
 
 from config import settings
+from menu_schema import MenuSchema
 
 BASE_DIR = Path(__file__).parent
 
@@ -58,7 +59,7 @@ def get_data_from_file(file_name: str) -> tuple:
     :return: tuple([element1...])
     """
     full_file_data = []
-    document = pdf_open("espn_bet.pdf")
+    document = pdf_open(file_name)
     for page in document:
         page_data = page.get_text("blocks")
         for block in page_data:
@@ -77,6 +78,133 @@ def get_data_from_file(file_name: str) -> tuple:
     return tuple(full_file_data)
 
 
+def get_grouped_data_by_categories(file_data: tuple) -> dict[str:list]:
+    grouped_data = {}
+    new_category = None
+
+    for item in file_data:
+        if "served" in item or "Contains" in item:
+            continue
+        elif "ENERGY" in item or item.isupper() and "$" not in item:
+            new_category = item
+            grouped_data.update({item: []})
+        else:
+            grouped_data[new_category].append(item)
+    logger.info(
+        f"{get_grouped_data_by_categories.__name__} Successfully grouped data by categories"
+    )
+    return grouped_data
+
+
+def get_jumbo_chicken(grouped_data: dict) -> dict[str:list]:
+    """
+    Function for split dishes in AIN’T NO THING BUT A CHICKEN…
+    :param grouped_data: dict with category : dishes data
+    :return: dict with current main category
+    """
+    full_category = "AIN’T NO THING BUT A CHICKEN…"
+    sub_category = "JUMBO CHICKEN WINGS BREADED CHICKEN TENDERS"
+    dishes_list = grouped_data.get(sub_category)
+    split_sub_category = sub_category.split("WINGS")
+    jumbo = split_sub_category[0].strip()
+    breaded = split_sub_category[-1].strip()
+    split_index = len(dishes_list) - 2
+    jumbo_dishes = [f"{jumbo} {dish}" for dish in dishes_list[:split_index]]
+    breaded_dishes = [f"{breaded} {dish}" for dish in dishes_list[:split_index]]
+    logger.info(f"{get_jumbo_chicken.__name__} Successfully grouped chicken wings data")
+    return {full_category: [*jumbo_dishes, *breaded_dishes]}
+
+
+def clean_raw_text(raw_data: str) -> tuple:
+    """
+    Function for find lists in gpt response
+    :param raw_data: text
+    :return: tuple([dish1, dish2])
+    """
+    convert_str = []
+    try:
+        pattern = re.compile(r"\[.*?\]", re.DOTALL)
+        matches_data = pattern.findall(raw_data)
+        convert_str = [
+            x.strip().replace("'", "") for x in matches_data[-1].strip("[]").split(",")
+        ]
+        logger.info(f"{clean_raw_text.__name__} Successfully find data in raw text.")
+
+    except IndexError:
+        logger.error(f"{clean_raw_text.__name__} The are not any elements in response.")
+    return tuple(convert_str)
+
+
+async def get_split_sauces_memu(grouped_data: dict) -> dict[str:list]:
+    """
+    Function for separate sauces in sauces menu.
+    Make request to gpt for get elements list.
+    :param grouped_data: dict {category: [dishes list]}
+    :return: category: [dishes list]
+    """
+    categories = list(grouped_data.keys())
+    sauces_data = [
+        item
+        for item in categories
+        if "SIGNATURE SAUCES" in item or "BACON BOURBON" in item
+    ]
+    raw_sauces_data = await chat_gpt_request(data_set=sauces_data)
+    try:
+        cleaned_data = clean_raw_text(raw_data=raw_sauces_data)
+        logger.info(f"{get_split_sauces_memu.__name__} Successfully split sauces.")
+        return {cleaned_data[0]: cleaned_data[1:]}
+    except IndexError:
+        logger.error(f"{get_split_sauces_memu.__name__} The are not any sauces.")
+        return {}
+
+
+async def get_split_flights(grouped_data: dict) -> dict:
+    """
+    Function for separate amount of wings in element.
+    Make request to gpt for separate data.
+    :param grouped_data: dict {category: [dishes list]}
+    :return: category: [dishes list]
+    """
+    flights_data = grouped_data.get("FLIGHTS")
+    raw_wings_data = await chat_gpt_request(data_set=flights_data)
+    cleaned_data = clean_raw_text(raw_data=raw_wings_data)
+    logger.info(f"{get_split_flights.__name__} Successfully split dishes in FLIGHTS.")
+    return {"FLIGHTS": cleaned_data}
+
+
+def get_energy_drinks_menu(grouped_data: dict) -> dict:
+    """
+    Function for grouping energy drinks.
+    Find category and group energy drinks
+    :param grouped_data: dict {category: [dishes list]}
+    :return: category: [dishes list]
+    """
+    keys_list = list(grouped_data.keys())
+    index_energy = keys_list.index("ENERGY $X")
+    energy_drinks_data = keys_list[index_energy:]
+    category_name = energy_drinks_data.pop(0).replace(" $X", "")
+    logger.info(
+        f"{get_energy_drinks_menu.__name__} Successfully grouped {len(energy_drinks_data)} energy drinks."
+    )
+    return {category_name: energy_drinks_data}
+
+
+def get_join_rows(grouped_data: dict, cat_name: str) -> dict:
+    """
+    Function for join description to dish name
+    :param grouped_data: dict {category: [dishes list]}
+    :param cat_name: Name of category need for get data by key
+    :return: category: [dishes list]
+    """
+    cat_data = grouped_data.get(cat_name)
+    joined_rows = [
+        " ".join([cat_data[index], cat_data[index + 1]])
+        for index in range(0, len(cat_data), 2)
+    ]
+    logger.info(f"{get_join_rows.__name__} Successfully joined data.")
+    return {cat_name: joined_rows}
+
+
 async def main() -> None:
     """
     Main function with algorithm of work with files
@@ -86,12 +214,49 @@ async def main() -> None:
 
     for file in files_in_dir:
         file_data = get_data_from_file(file_name=file)
+        raw_grouped_data_by_categories = get_grouped_data_by_categories(
+            file_data=file_data
+        )
 
+        jumbo_chicken = get_jumbo_chicken(grouped_data=raw_grouped_data_by_categories)
+        sauces = await get_split_sauces_memu(
+            grouped_data=raw_grouped_data_by_categories
+        )
+        flights = await get_split_flights(grouped_data=raw_grouped_data_by_categories)
+        cocktails_update = get_join_rows(
+            grouped_data=raw_grouped_data_by_categories, cat_name="SIGNATURE COCKTAILS"
+        )
+        zero_proof_update = get_join_rows(
+            grouped_data=raw_grouped_data_by_categories, cat_name="ZERO PROOF"
+        )
+        energy_drinks = get_energy_drinks_menu(
+            grouped_data=raw_grouped_data_by_categories
+        )
+        grouped_data_without_empty_cat = {
+            category: dishes
+            for category, dishes in raw_grouped_data_by_categories.items()
+            if dishes and "JUMBO CHICKEN" not in category
+        }
+        grouped_data_without_empty_cat.update(
+            {
+                **jumbo_chicken,
+                **sauces,
+                **flights,
+                **cocktails_update,
+                **zero_proof_update,
+                **energy_drinks,
+            }
+        )
+        transform_menu_data = [
+            MenuSchema(category=category, dish=dish).split_dish_data
+            for category, dishes in grouped_data_without_empty_cat.items()
+            for dish in dishes
+        ]
         file_name = file.name
         with open(
             f"complete_{file_name.split('.')[0]}_menu.json", "w", encoding="utf-8"
         ) as j_file:
-            json.dump(file_data, j_file, ensure_ascii=False, indent=4)
+            json.dump(transform_menu_data, j_file, ensure_ascii=False, indent=4)
             logger.info(
                 f"Successfully saved complete_{file_name.split('.')[0]}_menu.json file to {BASE_DIR}"
             )
